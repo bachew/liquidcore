ef51286c1d283ca95797e1c42f3b38ad2177c6cc
Chew Boon Aik <bachew@gmail.com>
2024-09-22 10:04:51 +0800

    Rename INCLUDE_MANUAL_TESTS -> manual_test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 from liquidcore import logconf
-from liquidcore.constants import ENV_INCLUDE_MANUAL_TESTS
 from pathlib import Path
 import os
 import pytest
@@ -14,9 +13,7 @@ def tmp_dir(tmpdir):
 
 
 def manual_test(func):
-    include = os.environ.get(ENV_INCLUDE_MANUAL_TESTS)
-
-    if include:
+    if os.environ.get('manual_test') == '1':
         return func
 
     return pytest.mark.skip('manual test')(func)
